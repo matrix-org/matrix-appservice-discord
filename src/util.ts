023a98fdb702841cf2ea7d87c5db210d3962500a
Will Hunt <half-shot@molrams.com>
2017-02-18 15:12:47 +0000

    Add Util class, for uploading content to Matrix

---
+++ src/util.ts
@@ -0,0 +1,73 @@
+import * as http from "http";
+import * as https from "https";
+import { Bridge, Intent } from "matrix-appservice-bridge";
+import { Buffer } from "buffer";
+import * as log from "npmlog";
+import * as mime from "mime";
+
+export class Util {
+  /**
+   * uploadContentFromUrl - Upload content from a given URL to the homeserver
+   * and return a MXC URL.
+   */
+  public static uploadContentFromUrl(bridge: Bridge, url: string, id: string | Intent, name: string): Promise<any> {
+    let contenttype;
+    let size;
+    id = id || null;
+    name = name || null;
+    return new Promise((resolve, reject) => {
+      let ht;
+      if (url.startsWith("https")) {
+        ht = https;
+      } else {
+        ht = http;
+      }
+      const req = ht.get( url, (res) => {
+        let buffer = Buffer.alloc(0);
+
+        if (res.headers.hasOwnProperty("content-type")) {
+          contenttype = res.headers["content-type"];
+        } else {
+          log.verbose("UploadContent", "No content-type given by server, guessing based on file name.");
+          contenttype = mime.lookup(url);
+        }
+
+        if (name === null) {
+          let names = url.split("/");
+          name = names[names.length - 1];
+        }
+
+        res.on("data", (d) => {
+          buffer = Buffer.concat([buffer, d]);
+        });
+
+        res.on("end", () => {
+          resolve(buffer);
+        });
+      });
+      req.on("error", (err) => {
+        reject(`Failed to download. ${err.code}`);
+      });
+    }).then((buffer: Buffer) => {
+      size = buffer.length;
+      if (id === null || typeof id === "string") {
+        id = bridge.getIntent(id);
+      }
+      return id.getClient().uploadContent(buffer, {
+        name,
+        type: contenttype,
+        onlyContentUri: true,
+        rawResponse: false,
+      });
+    }).then((contentUri) => {
+      log.verbose("UploadContent", "Media uploaded to %s", contentUri);
+      return {
+        mxc_url: contentUri,
+        size,
+      };
+    }).catch((reason) => {
+      log.error("UploadContent", "Failed to upload content:\n%s", reason);
+      throw reason;
+    });
+  }
+}
