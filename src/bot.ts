9a04326d11d1b2b1b547f1b9b43b67664610f0f2
Will Hunt <half-shot@molrams.com>
2017-09-10 15:52:18 +0100

    Try to match mentions better.

--- src/bot.ts
+++ src/bot.ts
@@ -283,7 +283,7 @@ export class DiscordBot {
 
   private HandleMentions(body: string, members: Discord.GuildMember[]): string {
     for (const member of members) {
-      body = body.replace(new RegExp(escapeStringRegexp(member.displayName), "g"), `<@!${member.id}>`);
+      body = body.replace(new RegExp(`(^| |\\t)${escapeStringRegexp(member.displayName)}` , "mg"), ` <@!${member.id}>`);
     }
     return body;
   }
