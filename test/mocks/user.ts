348dcb27bf25f0a46918b0f62eb3f5e1a2754387
Will Hunt <half-shot@molrams.com>
2017-09-18 00:45:10 +0100

    Fix more mention match cases for issue 46

--- test/mocks/user.ts
+++ test/mocks/user.ts
@@ -1,6 +1,7 @@
 export class MockUser {
   public id = "";
   public username: string;
+  public discriminator: string;
   constructor(id: string, username: string = "") {
     this.id = id;
     this.username = username;
