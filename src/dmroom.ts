7a1e8e2b3a4805a1e50a83581ef83c7b3052151a
Will Hunt <will@half-shot.uk>
2018-06-28 11:19:26 +0100

    HandleInvite code for DMRoom

--- src/dmroom.ts
+++ src/dmroom.ts
@@ -1,20 +1,38 @@
-import { TextBasedChannel, Snowflake, Invite, Client } from "discord.js";
-import { MatrixRoom } from "matrix-appservice-bridge";
-
-export interface InviteAction {
-
-}
+import { Snowflake, Client, TextChannel, Collection, GuildMember } from "discord.js";
+import { MatrixRoom , Intent} from "matrix-appservice-bridge";
+import * as log from "npmlog";
 
 export class DMRoom {
     private matrixMembers: Set<string>;
-    private discordMembers: Set<string>;
+    private discordMembers: Collection<string, GuildMember>;
     
     constructor(private id: Snowflake, private matrixRoom: MatrixRoom) {
 
     }
 
-    public HydrateRoomWithClient(discordClient: Client) {
+    public HydrateRoomWithDiscord(discordClient: Client) {
+        const channel = discordClient.channels.get(this.id);
+        if (channel === undefined) {
+            log.warn("DMRoom", "HydrateRoomWithDiscord was given a client that didn't have the channel.");
+            throw new Error("Channel not found for client!");
+        }
+        if (!["dm", "group"].includes(channel.type)) {
+            log.warn("DMRoom", "HydrateRoomWithDiscord was given a channel that was not a dm|group.");
+            throw new Error("Channel is not a dm|group");
+        }
+        this.discordMembers = (channel as TextChannel).members;
+        log.verbose("DMRoom", `${this.id} has ${this.discordMembers.size} discord members`);
+    }
 
+    public async HydrateRoomWithMatrix(matrixClient: Intent): Promise<void>{
+        const roomState: any[] = await matrixClient.roomState(this.matrixRoom.getId());
+        roomState.forEach(element => {
+            if (element.type === "m.room.member" &&
+                element.content.membership === "join") {
+                this.matrixMembers.add(element.sender);
+            }
+        });
+        log.info("DMRoom", `${this.matrixRoom.getId()} has ${this.matrixMembers.size} matrix members (including virtual users)`);
     }
 
     public async OnMatrixEvent(event: any) {
