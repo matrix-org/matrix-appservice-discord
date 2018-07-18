b1190b40d6dff9222d6bcb6cdcca1c1906a66223
Will Hunt <will@half-shot.uk>
2018-07-18 17:12:15 +0100

    Presence should use User rather than GuildMember

--- test/mocks/user.ts
+++ test/mocks/user.ts
@@ -1,9 +1,16 @@
+import { Presence } from "discord.js";
+
 export class MockUser {
   public id = "";
   public username: string;
   public discriminator: string;
+  public presence: Presence;
   constructor(id: string, username: string = "") {
     this.id = id;
     this.username = username;
+  }
+
+  public MockSetPresence(presence: Presence) {
+      this.presence = presence;
   }
 }
