0cbd077fd9a597e2f61b6b5d373b7b1833771e6c
Andrew Morgan <andrew@amorgan.xyz>
2018-10-04 14:50:37 +0200

    Fix message and add tests

--- src/matrixeventprocessor.ts
+++ src/matrixeventprocessor.ts
@@ -98,15 +98,17 @@ export class MatrixEventProcessor {
           body = "\\" + body;
         }*/
 
+        // Replace /me with * username ...
         if (event.content.msgtype === "m.emote") {
-          if (profile &&
-              profile.displayname &&
-              profile.displayname.length >= MIN_NAME_LENGTH &&
-              profile.displayname.length <= MAX_NAME_LENGTH) {
-            body = `* \* ${profile.displayname} ${body}*`;
-          } else {
-            body = `* ${body} *`;
-          }
+            if (profile &&
+                profile.displayname &&
+                profile.displayname.length >= MIN_NAME_LENGTH &&
+                profile.displayname.length <= MAX_NAME_LENGTH) {
+                body = body.replace(/\/me /g, "");
+                body = `* \\* ${profile.displayname} ${body} *`;
+            } else {
+                body = `* ${body} *`;
+            }
         }
 
         // replace <del>blah</del> with ~~blah~~
