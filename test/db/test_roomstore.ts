5e0704a433f49732f4851733612f17410a2a7c4e
Travis Ralston <travpc@gmail.com>
2019-02-09 03:27:34 +0000

    Update test/db/test_roomstore.ts
    Co-Authored-By: Half-Shot <will@half-shot.uk>

--- test/db/test_roomstore.ts
+++ test/db/test_roomstore.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2017, 2018 matrix-appservice-discord
+Copyright 2019 matrix-appservice-discord
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
