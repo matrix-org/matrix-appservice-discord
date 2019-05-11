356849c265243cd664eac9a27922ca296ec44cb3
Sorunome <mail@sorunome.de>
2019-05-11 11:08:45 +0200

    add new tests

--- test/test_util.ts
+++ test/test_util.ts
@@ -60,35 +60,72 @@ describe("Util", () => {
             Chai.assert.equal(args[1], "arg2");
         });
     });
-    describe("ParseCommand", () => {
-        it("parses commands", async () => {
-            const actions: ICommandActions = {
-                action: {
-                    params: ["param1", "param2"],
-                    run: async ({param1, param2}) => {
-                        return `param1: ${param1}\nparam2: ${param2}`;
-                    },
+    describe("Command Stuff", () => {
+        const actions: ICommandActions = {
+            action: {
+                description: "floof",
+                help: "Fox goes floof!",
+                params: ["param1", "param2"],
+                run: async ({param1, param2}) => {
+                    return `param1: ${param1}\nparam2: ${param2}`;
                 },
-            };
-            const parameters: ICommandParameters = {
-                param1: {
-                    get: async (param: string) => {
-                        return "param1_" + param;
-                    },
+            },
+        };
+        const parameters: ICommandParameters = {
+            param1: {
+                description: "1",
+                get: async (param: string) => {
+                    return "param1_" + param;
                 },
-                param2: {
-                    get: async (param: string) => {
-                        return "param2_" + param;
-                    },
+            },
+            param2: {
+                description: "2",
+                get: async (param: string) => {
+                    return "param2_" + param;
                 },
-            };
-            const retStr = await Util.ParseCommand(
-                "!fox",
-                "!fox action hello world",
-                actions,
-                parameters,
-            );
-            expect(retStr).equal("param1: param1_hello\nparam2: param2_world");
+            },
+        };
+        describe("ParseHelpMessage", () => {
+            it("parses general help message", async () => {
+                const {command, args} = Util.MsgToArgs("!fox help", "!fox");
+                const retStr = await Util.ParseHelpMessage(
+                    "!fox",
+                    actions,
+                    parameters,
+                    args,
+                );
+                expect(retStr).to.equal(
+`Available Commands:
+ - \`!fox action <param1> <param2>\`: floof
+
+Parameters:
+ - \`<param1>\`: 1
+ - \`<param2>\`: 2
+`);
+            });
+            it("parses specific help message", async () => {
+                const {command, args} = Util.MsgToArgs("!fox help action", "!fox");
+                const retStr = await Util.ParseHelpMessage(
+                    "!fox",
+                    actions,
+                    parameters,
+                    args,
+                );
+                expect(retStr).to.equal(
+`\`!fox action <param1> <param2>\`: floof
+Fox goes floof!`);
+            });
+        });
+        describe("ParseCommand", () => {
+            it("parses commands", async () => {
+                const retStr = await Util.ParseCommand(
+                    "!fox",
+                    "!fox action hello world",
+                    actions,
+                    parameters,
+                );
+                expect(retStr).equal("param1: param1_hello\nparam2: param2_world");
+            });
         });
     });
     describe("GetMxidFromName", () => {
@@ -192,6 +229,86 @@ describe("Util", () => {
             const t = Date.now();
             await Util.DelayedPromise(DELAY_FOR);
             expect(Date.now()).to.be.greaterThan(t + DELAY_FOR - 1);
+        });
+    });
+    describe("CheckMatrixPermission", () => {
+        const PERM_LEVEL = 50;
+        it("should deny", async () => {
+            const ret = await Util.CheckMatrixPermission(
+                {
+                    getStateEvent: async () => {
+                        return {
+                            blah: {
+                                blubb: PERM_LEVEL,
+                            },
+                        };
+                    },
+                } as any,
+                "@user:localhost",
+                "",
+                PERM_LEVEL,
+                "blah",
+                "blubb",
+            );
+            expect(ret).to.be.false;
+        });
+        it("should allow cat/subcat", async () => {
+            const ret = await Util.CheckMatrixPermission(
+                {
+                    getStateEvent: async () => {
+                        return {
+                            blah: {
+                                blubb: PERM_LEVEL,
+                            },
+                            users: {
+                                "@user:localhost": PERM_LEVEL,
+                            },
+                        };
+                    },
+                } as any,
+                "@user:localhost",
+                "",
+                PERM_LEVEL,
+                "blah",
+                "blubb",
+            );
+            expect(ret).to.be.true;
+        });
+        it("should allow cat", async () => {
+            const ret = await Util.CheckMatrixPermission(
+                {
+                    getStateEvent: async () => {
+                        return {
+                            blah: PERM_LEVEL,
+                            users: {
+                                "@user:localhost": PERM_LEVEL,
+                            },
+                        };
+                    },
+                } as any,
+                "@user:localhost",
+                "",
+                PERM_LEVEL,
+                "blah",
+            );
+            expect(ret).to.be.true;
+        });
+        it("should allow based on default", async () => {
+            const ret = await Util.CheckMatrixPermission(
+                {
+                    getStateEvent: async () => {
+                        return {
+                            blah: PERM_LEVEL,
+                            users_default: PERM_LEVEL,
+                        };
+                    },
+                } as any,
+                "@user:localhost",
+                "",
+                PERM_LEVEL,
+                "blah",
+            );
+            expect(ret).to.be.true;
         });
     });
 });
