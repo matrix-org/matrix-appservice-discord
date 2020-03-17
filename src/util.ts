c6e2b1f3b8f7dbdd35caa6e33a26407eb17b3f91
David Vo <auscompgeek@users.noreply.github.com>
2020-03-17 12:22:14 +1100

    Make type inference happier in DownloadFile

--- src/util.ts
+++ src/util.ts
@@ -63,13 +63,11 @@ export class Util {
      */
     public static async DownloadFile(url: string): Promise<Buffer> {
         return new Promise((resolve, reject) => {
-            let ht;
+            let get = http.get;
             if (url.startsWith("https")) {
-                ht = https;
-            } else {
-                ht = http;
+                get = https.get;
             }
-            const req = ht.get((url), (res) => {
+            const req = get((url), (res) => {
                 let buffer = Buffer.alloc(0);
                 if (res.statusCode !== HTTP_OK) {
                     reject(`Non 200 status code (${res.statusCode})`);
@@ -84,7 +82,7 @@ export class Util {
                 });
             });
             req.on("error", (err) => {
-                reject(`Failed to download. ${err.code}`);
+                reject(`Failed to download. ${err}`);
             });
         }) as Promise<Buffer>;
     }
