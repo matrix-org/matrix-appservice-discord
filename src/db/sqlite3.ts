7b35503ac9ab3c10fbe9a7236db4dd7195ff4e76
Will Hunt <will@half-shot.uk>
2020-11-03 15:08:06 +0000

    Use correct types for sqlit3

--- src/db/sqlite3.ts
+++ src/db/sqlite3.ts
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import * as Database from "better-sqlite3";
+import * as BetterSQLite3 from "better-sqlite3";
+import { Database } from "better-sqlite3";
 import { Log } from "../log";
 import { IDatabaseConnector, ISqlCommandParameters, ISqlRow } from "./connector";
 const log = new Log("SQLite3");
@@ -27,7 +28,8 @@ export class SQLite3 implements IDatabaseConnector {
 
     public async Open() {
         log.info(`Opening ${this.filename}`);
-        this.db = new Database(this.filename);
+        
+        this.db = new BetterSQLite3(this.filename);
     }
 
     public async Get(sql: string, parameters?: ISqlCommandParameters): Promise<ISqlRow|null> {
@@ -42,7 +44,7 @@ export class SQLite3 implements IDatabaseConnector {
 
     public async Run(sql: string, parameters?: ISqlCommandParameters): Promise<void> {
         log.silly("Run:", sql);
-        return this.db.prepare(sql).run(parameters || []);
+        await this.db.prepare(sql).run(parameters || []);
     }
 
     public async Close(): Promise<void> {
@@ -51,6 +53,6 @@ export class SQLite3 implements IDatabaseConnector {
 
     public async Exec(sql: string): Promise<void> {
         log.silly("Exec:", sql);
-        return this.db.exec(sql);
+        await this.db.exec(sql);
     }
 }
