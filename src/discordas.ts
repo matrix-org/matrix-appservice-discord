a65bc62328c2d2c2ae177063ee3a89c6029d797b
Christian Paul <christianp@matrix.org>
2020-12-07 13:35:52 +0100

    Import express types

--- src/discordas.ts
+++ src/discordas.ts
@@ -26,6 +26,7 @@ import * as usage from "command-line-usage";
 import * as uuid from "uuid/v4";
 import { IMatrixEvent } from "./matrixtypes";
 import { MetricPeg, PrometheusBridgeMetrics } from "./metrics";
+import { Response } from "express";
 
 const log = new Log("DiscordAS");
 
@@ -176,8 +177,12 @@ async function run() {
     const roomhandler = discordbot.RoomHandler;
     const eventProcessor = discordbot.MxEventProcessor;
 
-    // tslint:disable-next-line:no-any
-    appservice.expressAppInstance.get("/health", (_, res: any) => {
+    // 2020-12-07: If this fails to build in TypeScript with
+    // "Namespace 'serveStatic' has no exported member 'RequestHandlerConstructor'.",
+    // remove @types/express-serve-static-core and @types/serve-static from yarn.lock
+    // and run yarn.
+    // See https://github.com/DefinitelyTyped/DefinitelyTyped/issues/49595
+    appservice.expressAppInstance.get("/health", (_, res: Response) => {
         res.status(201).send("");
     });
 
