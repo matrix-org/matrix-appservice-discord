a46989a2d1b1b69c70dd09024a68f1b46c5339fe
Andrew Ferrazzutti <andrewf@element.io>
2023-09-11 08:45:23 -0400

    Update matrix-appservice-bridge (#897)
    * Update matrix-appservice-bridge
    
    Fixes #896
    
    * Drop support for Node 16, default to Node 18
    
    Required for updating to the latest matrix-appservice-bridge
    
    * Update better-sqlite3
    
    Required for Node 20 support
    
    * Update changelog.d/897.removal
    
    Co-authored-by: Will Hunt <will@half-shot.uk>
    Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>
    
    ---------
    
    Signed-off-by: Andrew Ferrazzutti <andrewf@element.io>
    Co-authored-by: Will Hunt <will@half-shot.uk>

--- src/bot.ts
+++ src/bot.ts
@@ -1233,7 +1233,10 @@ export class DiscordBot {
 
     private async onUserActivityChanged(state: UserActivityState) {
         for (const userId of state.changed) {
-            await this.store.storeUserActivity(userId, state.dataSet.users[userId]);
+            const activity = state.dataSet.get(userId);
+            if (activity) {
+                await this.store.storeUserActivity(userId, activity);
+            }
         }
         log.verbose(`Checking bridge limits (${state.activeUsers} active users)`);
         this.bridgeBlocker?.checkLimits(state.activeUsers).catch(err => {
